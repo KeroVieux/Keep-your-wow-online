87845a5d3fe12c491ade39235d4e4c2438a452d3
keroVieux <308680@gmail.com>
2020-02-21 02:27:18 +0800

    improved random keys selected

--- actions.py
+++ actions.py
@@ -3,12 +3,20 @@ from reboot import *
 
 keyboard = Controller()
 
+
+def knuth_shuffle(list):
+    for i in range(len(list) - 1, 0, -1):
+        p = random.randrange(0, i + 1)
+        list[i], list[p] = list[p], list[i]
+    return list
+
+
 letters = ['a', 'd', 'f', 'z', 'x', '6', '7', '8']
 
 
 def random_actions():
     time.sleep(random.randint(5, 30))
-    letters_shuffle = random.shuffle(letters)
+    letters_shuffle = knuth_shuffle(letters)
     keyboard.press(letters_shuffle[0])
     keyboard.release(letters_shuffle[0])
 
